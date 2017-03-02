f86b202371eabc486057cc9f10f433147aadd5a1
Siyu Kat Wang <siyukw@gmail.com>
2017-03-01 19:19:49 -0800

    added getData in main and changed object class

--- src/whatGradeMain.java
+++ src/whatGradeMain.java
@@ -1,12 +1,35 @@
 // the file that is being executed
+
+import java.util.Scanner;
+
 public class whatGradeMain {
     public static void main(String[] args) {
         intro();
-
+        Scanner console = new Scanner(System.in);
+        if(prompt("Would you like to start a new grade calculation?", console)) {
+            whatGradeWeights temp = new whatGradeWeights();
+            temp = getData(temp, console);
+        }
     }
 
     public static void intro() {
         System.out.println("Hi, welcome to whatGradeDoINeed!");
         System.out.println("You can use this app to find out how much can you fuck up your next assignment/test");
     }
+
+    public static boolean prompt(String prompt, Scanner console) {
+        System.out.println();
+        System.out.print(prompt + " (y/n)? ");
+        String response = console.nextLine().trim().toLowerCase();
+        return response.equals("y");
+    }
+
+    public static whatGradeWeights getData(whatGradeWeights temp, Scanner console) {
+        System.out.println("What is the name of the class?");
+        temp.setName(console.next());
+        System.out.println("What is the percentage of your quiz score out of your total grade? (x%)");
+        temp.setQzPc(console.nextDouble());
+        return temp;
+    }
+
 }
--- src/whatGradeWeights.java
+++ src/whatGradeWeights.java
@@ -1,17 +1,41 @@
+import java.util.DoubleSummaryStatistics;
+
 // create whatGrade objects to store information about a class
+// name, hwpc, quizpc, otherpc, exampc
 public class whatGradeWeights {
     private String name;
     private double quizPercentage;
     private double hwPercentage;
-    private double participationPercentage;
     private double otherPercentage;
+    private double examPercentage;
+
+    public whatGradeWeights() {
+        this(null, 0, 0, 0, 0);
+    }
 
     public whatGradeWeights(String name, double quizPercentage, double hwPercentage,
-                            double participationPercentage, double otherPercentage) {
+                            double otherPercentage, double examPercentage) {
         this.name = name;
         this.quizPercentage = quizPercentage;
         this.hwPercentage = hwPercentage;
-        this.participationPercentage = participationPercentage;
+        this.examPercentage = examPercentage;
         this.otherPercentage = otherPercentage;
     }
+
+    public void setName(String input) {
+        name = input;
+    }
+    public void setQzPc(Double input) {
+        quizPercentage = input;
+    }
+    public void setHwPc(Double input) {
+        hwPercentage = input;
+    }
+    public void setOPc(Double input) {
+        otherPercentage = input;
+    }
+    public void setExPc(Double input) {
+        examPercentage = input;
+    }
+
 }
